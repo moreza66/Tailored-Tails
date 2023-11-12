857807d17150514387304ec6fc8ac6b42dfb5076
“momemar” <mmemar@logisx.net>
2023-11-12 12:37:42 -0600

    clean the code

--- Backend/server.js
+++ Backend/server.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const path = require("path");
+const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const path = require("path")
 
-const IndexRoute = require("./Routers/index");
-const connectDatabase = require("./Helpers/database/connectDatabase");
-const customErrorHandler = require("./Middlewares/Errors/customErrorHandler");
+const IndexRoute = require("./Routers/index")
+const connectDatabase = require("./Helpers/database/connectDatabase")
+const customErrorHandler = require("./Middlewares/Errors/customErrorHandler")
 
 dotenv.config({
     path: './Config/config.env'
-});
+})
 
-connectDatabase();
+connectDatabase()
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+const nodemailer = require('nodemailer');
+app.use(express.json())
+app.use(cors())
 
-app.use("/", IndexRoute);
-app.use(customErrorHandler);
+app.use("/", IndexRoute)
+
+app.use(customErrorHandler)
 
 const PORT = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, "public")));
-const nodemailer = require('nodemailer');
+
+app.use(express.static(path.join(__dirname, "public")))
 
 app.post('/api/send-email', (req, res) => {
     const { name, email, message } = req.body;
@@ -61,12 +63,13 @@ app.post('/api/send-email', (req, res) => {
 });
 
 const server = app.listen(PORT, () => {
-    console.log(`Server running on port  ${PORT} : ${process.env.NODE_ENV}`);
-});
+
+    console.log(`Server running on port  ${PORT} : ${process.env.NODE_ENV}`)
+
+})
 
 process.on("unhandledRejection", (err, promise) => {
-    console.log(`Logged Error : ${err}`);
-    server.close(() => process.exit(1));
-});
+    console.log(`Logged Error : ${err}`)
 
-
+    server.close(() => process.exit(1))
+})
