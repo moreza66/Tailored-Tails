e27c059684df27cdcf4a2673262c27ca5095fece
“momemar” <mmemar@logisx.net>
2023-11-12 12:34:05 -0600

    initial setup

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+
+const IndexRoute = require("./Routers/index");
+const connectDatabase = require("./Helpers/database/connectDatabase");
+const customErrorHandler = require("./Middlewares/Errors/customErrorHandler");
+
+dotenv.config({
+    path: './Config/config.env'
+});
+
+connectDatabase();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/", IndexRoute);
+app.use(customErrorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, "public")));
