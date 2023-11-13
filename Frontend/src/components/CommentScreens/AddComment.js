2ee6901786108083a66ba5941ec0a2b7caf2fae1
“momemar” <mmemar@logisx.net>
2023-11-13 16:31:17 -0600

    Add state hooks and useEffect for data fetching in AddComment

--- Frontend/src/components/CommentScreens/AddComment.js
+++ Frontend/src/components/CommentScreens/AddComment.js
@@ -6,3 +6,19 @@ import { IoAdd } from 'react-icons/io5';
 import Loader from "../GeneralScreens/Loader";
 import { useNavigate } from 'react-router-dom';
 import '../../Css/AddComment.css';
+const AddComment = ({ setSidebarShowStatus, slug, getStoryComments, activeUser, count }) => {
+    const navigate = useNavigate();
+    const textareaRef = useRef(null);
+    const [star, setStar] = useState(0);
+    const [starCurrentVal, setStarCurrentVal] = useState(0);
+    const [content, setContent] = useState('');
+    const [success, setSuccess] = useState('');
+    const [error, setError] = useState('');
+    const [showStatus, setShowStatus] = useState(true);
+    const [loading, setLoading] = useState(true);
+    const [storyUser, setStoryUser] = useState("");
+
+    useEffect(() => {
+        // useEffect content here
+    }, [slug, setLoading]);
+
