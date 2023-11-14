c091bc9b667533deb701c1c70bef658c6952fb50
“momemar” <mmemar@logisx.net>
2023-11-14 16:28:41 -0600

    Initialize ChangePassword component with imports

---
+++ Frontend/src/components/ProfileScreens/ChangePassword.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import '../../Css/ChangePassword.css'
+import { IoEyeOffOutline, IoEyeOutline } from 'react-icons/io5'
+
+const ChangePassword = () => {
+    // Rest of the code will be added in the following commits...
+}
