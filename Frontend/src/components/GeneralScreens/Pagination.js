171d2d96388fb828a552bdc2571e3ca73c7cb022
“momemar” <mmemar@logisx.net>
2023-11-13 22:49:54 -0600

    Define numberRange function and initialize Pagination component structure

--- Frontend/src/components/GeneralScreens/Pagination.js
+++ Frontend/src/components/GeneralScreens/Pagination.js
@@ -2,3 +2,9 @@ import React from 'react';
 import '../../Css/Pagination.css';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { TiMinus } from 'react-icons/ti';
+const Pagination = ({ page, pages, changePage }) => {
+    function numberRange(start, end) {
+        return new Array(end - start).fill().map((d, i) => i + start);
+    }
+    // (Keep only the function definition and initialization of variables here)
+  };
