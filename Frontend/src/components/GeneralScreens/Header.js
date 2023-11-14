63f50b7bbea34cf5598450760d33be473aaba81d
“momemar” <mmemar@logisx.net>
2023-11-13 21:25:23 -0600

    Initialize Header component with necessary imports

---
+++ Frontend/src/components/GeneralScreens/Header.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import SearchForm from './SearchForm';
+import '../../Css/Header.css'
+import { RiPencilFill } from 'react-icons/ri'
+import { FaUserEdit } from 'react-icons/fa'
+import { BiLogOut } from 'react-icons/bi'
+import { BsBookmarks } from 'react-icons/bs'
+import SkeletonElement from '../Skeletons/SkeletonElement';
+import { AuthContext } from '../../Context/AuthContext';
+
+const Header = () => {
+    // Remaining parts will be committed in subsequent commits...
+}
+
+export default Header;
