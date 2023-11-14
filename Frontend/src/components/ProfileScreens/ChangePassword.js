e7e5048611e758d8c70f30b7ef4d076b8badbd8f
“momemar” <mmemar@logisx.net>
2023-11-14 16:29:11 -0600

    Add state management in ChangePassword component

--- Frontend/src/components/ProfileScreens/ChangePassword.js
+++ Frontend/src/components/ProfileScreens/ChangePassword.js
@@ -7,3 +7,13 @@ import { IoEyeOffOutline, IoEyeOutline } from 'react-icons/io5'
 const ChangePassword = () => {
     // Rest of the code will be added in the following commits...
 }
+    // Continuing from Part 1...
+    const [isRevealPass1, setIsRevealPass1] = useState(false);
+    const [isRevealPass2, setIsRevealPass2] = useState(false);
+    const [isRevealPass3, setIsRevealPass3] = useState(false);
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [oldPassword, setOldPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    // Rest of the code in subsequent commits...
