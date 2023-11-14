39b14c60e05f9d4bf5eb443d84cccd2c3e174a09
“momemar” <mmemar@logisx.net>
2023-11-13 22:59:18 -0600

    Add SearchForm component with navigation on search submission

--- Frontend/src/components/GeneralScreens/SearchForm.js
+++ Frontend/src/components/GeneralScreens/SearchForm.js
@@ -1,23 +1,38 @@
-import React, { useState } from 'react';
-import {BiSearchAlt2} from 'react-icons/bi';
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+import {BiSearchAlt2} from 'react-icons/bi'
+import {  useNavigate } from "react-router-dom";
 const SearchForm = () => {
     const [searchTerm, setSearchTerm] = useState("");
-    const navigate = useNavigate();
-  
-};
+    const navigate =useNavigate()
 
-const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (searchTerm) {
-        navigate(`/?search=${searchTerm}`);
+    const handleSubmit = async (e) => {
+        e.preventDefault() ; 
+        if(searchTerm){
+            navigate(`/?search=${searchTerm}`)
+        }
+
+        setSearchTerm("")
     }
-    setSearchTerm("");
-};
 
-return (
-    <form className="search-form" onSubmit={handleSubmit}>
-        // ... (rest of the form JSX)
-    </form>
-);
-export default SearchForm;
+  
+    return (
+     
+        <form
+            className="search-form"
+            onSubmit={handleSubmit}
+        >
+            <input
+                type="text"
+                name="search"
+                placeholder="Search..."
+                className=" m-4"
+                onChange={(e) => setSearchTerm(e.target.value)}
+                value={searchTerm}
+            />
+
+            <button type="submit" className={searchTerm.trim() ? 'searchBtn' : 'disBtn'}  ><i> <BiSearchAlt2/> </i> </button>
+        </form>
+    )
+}
+
+export default SearchForm
