376407b0cc3bbc841bb01921c29ddc6bc08e7076
“momemar” <mmemar@logisx.net>
2023-11-13 22:56:24 -0600

    Add state hook for searchTerm and navigate function in SearchForm

--- Frontend/src/components/GeneralScreens/SearchForm.js
+++ Frontend/src/components/GeneralScreens/SearchForm.js
@@ -1,3 +1,8 @@
 import React, { useState } from 'react';
 import {BiSearchAlt2} from 'react-icons/bi';
 import { useNavigate } from "react-router-dom";
+const SearchForm = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const navigate = useNavigate();
+  
+};
