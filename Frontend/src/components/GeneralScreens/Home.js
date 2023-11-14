0cf3f5e112f3fd374c26c6b65fe7eedb107805f2
“momemar” <mmemar@logisx.net>
2023-11-13 22:17:35 -0600

    Add state hooks and basic structure for Home component

--- Frontend/src/components/GeneralScreens/Home.js
+++ Frontend/src/components/GeneralScreens/Home.js
@@ -7,3 +7,14 @@ import CardStory from "../StoryScreens/CardStory";
 import NoStories from "../StoryScreens/NoStories";
 import Pagination from "./Pagination";
 import "../../Css/Home.css";
+const Home = () => {
+    const search = useLocation().search;
+    const searchKey = new URLSearchParams(search).get('search');
+    const [stories, setStories] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+    const [page, setPage] = useState(1);
+    const [pages, setPages] = useState(1);
+  
+    // ... (rest of the useEffect hooks and component logic)
+  };
