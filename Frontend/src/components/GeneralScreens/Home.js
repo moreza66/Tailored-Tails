d83113e67200c152a68ba73d8c452da8bda5a9cf
“momemar” <mmemar@logisx.net>
2023-11-13 22:16:54 -0600

    Setup imports and initial dependencies for Home component

---
+++ Frontend/src/components/GeneralScreens/Home.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { v4 as uuidv4 } from 'uuid';
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import SkeletonStory from "../Skeletons/SkeletonStory";
+import CardStory from "../StoryScreens/CardStory";
+import NoStories from "../StoryScreens/NoStories";
+import Pagination from "./Pagination";
+import "../../Css/Home.css";
