5a86baecdaaf4b9e7f4e0fbbd6493d0525725126
“momemar” <mmemar@logisx.net>
2023-11-13 22:22:52 -0600

    Implement Loader component with CSS styling

---
+++ Frontend/src/components/GeneralScreens/Loader.js
@@ -0,0 +1,11 @@
+import '../../Css/Loader.css'
+const Loader = () => {
+  return (
+     <div className="mask">
+         <div className="loader"></div>
+    </div>
+
+  );
+}
+
+export default Loader;
