fbf498db3ac4ff98c42177a1d573a45659b67fcf
“momemar” <mmemar@logisx.net>
2023-11-13 20:05:48 -0600

    Imports and Component Declaration

---
+++ Frontend/src/components/CommentScreens/CommentItem.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { FaStar } from 'react-icons/fa';
+import { MdOutlineWavingHand, MdWavingHand } from 'react-icons/md';
+import { BsThreeDots } from 'react-icons/bs';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const CommentItem = ({ comment, activeUser }) => {
+    // Rest of the code will go in the following parts...
