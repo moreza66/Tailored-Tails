1a8e46eabf56509e3275a626a4490739feacddf2
“momemar” <mmemar@logisx.net>
2023-11-15 12:07:30 -0600

    Set up state variables in ContactUs component

--- Frontend/src/components/ContactUs.js
+++ Frontend/src/components/ContactUs.js
@@ -5,3 +5,10 @@ import { useNavigate } from 'react-router-dom';
 const ContactUs = () => {
     // Additional code will be added in subsequent commits...
 }
+    // Continuing from Commit 1...
+    const [loading, setLoading] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+    // Further code will be added in subsequent commits...
