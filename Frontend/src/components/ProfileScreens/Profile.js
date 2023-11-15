dd0785418681279ee2d3b368297efce7aa173340
“momemar” <mmemar@logisx.net>
2023-11-14 21:03:30 -0600

    Initialize Profile component with imports

---
+++ Frontend/src/components/ProfileScreens/Profile.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState, useContext } from 'react';
+import axios from 'axios';
+import "../../Css/Profile.css"
+import { Link, useNavigate } from 'react-router-dom';
+import Loader from "../GeneralScreens/Loader";
+import { AuthContext } from '../../Context/AuthContext';
+import { FiArrowLeft } from 'react-icons/fi'
