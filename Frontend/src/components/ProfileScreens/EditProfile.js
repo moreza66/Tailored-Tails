ab4c1861662df9f922bc8aafbaf5a6a8ffa151f0
“momemar” <mmemar@logisx.net>
2023-11-14 20:58:28 -0600

    Initialize EditProfile component with state and context setup

--- Frontend/src/components/ProfileScreens/EditProfile.js
+++ Frontend/src/components/ProfileScreens/EditProfile.js
@@ -6,3 +6,13 @@ import Loader from "../GeneralScreens/Loader";
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthContext';
 import '../../Css/EditProfile.css'
+const EditProfile = () => {
+    const { activeUser, config } = useContext(AuthContext)
+    const [loading, setLoading] = useState(true)
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [photo, setPhoto] = useState('')
+    const [previousPhoto, setPreviousPhoto] = useState('')
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
