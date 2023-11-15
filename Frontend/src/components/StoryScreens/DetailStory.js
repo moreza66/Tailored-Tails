75827c2be39609e80ca76cc14378d1ecfc17ef32
“momemar” <mmemar@logisx.net>
2023-11-15 11:31:02 -0600

    Initialize DetailStory component with imports and basic structure

---
+++ Frontend/src/components/StoryScreens/DetailStory.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams, Link } from 'react-router-dom';
+import "../../Css/DetailStory.css";
+import Loader from '../GeneralScreens/Loader';
+import { FaRegHeart, FaHeart, FaRegComment } from 'react-icons/fa';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { FiEdit, FiArrowLeft } from 'react-icons/fi';
+import { BsBookmarkPlus, BsThreeDots, BsBookmarkFill } from 'react-icons/bs';
+import CommentSidebar from '../CommentScreens/CommentSidebar';
+
+const DetailStory = () => {
+    // Further code will be added in subsequent commits...
+}
