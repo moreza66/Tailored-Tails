fa61b72d9f4b8d9591e76e283050665e94713381
“momemar” <mmemar@logisx.net>
2023-11-15 12:09:43 -0600

    Initialize AuthContextProvider with imports and context setup

---
+++ Frontend/src/Context/AuthContext.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export const AuthContext = React.createContext();
+
+const AuthContextProvider = props => {
+    // Additional code will be added in subsequent commits...
+}
