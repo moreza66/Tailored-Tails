afa1628431fafaba6d677585f3ffc5d5b7d6b954
“momemar” <mmemar@logisx.net>
2023-11-15 11:21:09 -0600

    Set up AddStory component with necessary imports

---
+++ Frontend/src/components/StoryScreens/AddStory.js
@@ -0,0 +1,13 @@
+import React, { useRef, useContext, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { AuthContext } from "../../Context/AuthContext";
+import { AiOutlineUpload } from 'react-icons/ai';
+import { FiArrowLeft } from 'react-icons/fi';
+import '../../Css/AddStory.css';
+
+const AddStory = () => {
+    // Further code will be added in subsequent commits...
+}
