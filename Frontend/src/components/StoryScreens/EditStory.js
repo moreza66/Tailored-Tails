1fd81954fcef53eb6d4efcd822b587270fb185ec
“momemar” <mmemar@logisx.net>
2023-11-15 11:36:08 -0600

    Set up EditStory component with imports and basic structure

---
+++ Frontend/src/components/StoryScreens/EditStory.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState, useRef, useContext } from 'react';
+import axios from 'axios';
+import Loader from '../GeneralScreens/Loader';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { useNavigate, useParams, Link } from 'react-router-dom';
+import { AuthContext } from "../../Context/AuthContext";
+import { AiOutlineUpload } from 'react-icons/ai';
+import '../../Css/EditStory.css';
+
+const EditStory = () => {
+    // Further code will be added in subsequent commits...
+}
