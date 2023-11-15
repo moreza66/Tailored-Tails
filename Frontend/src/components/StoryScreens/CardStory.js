ed85bd7b48eb781d464efcb98edaa937f468ba74
“momemar” <mmemar@logisx.net>
2023-11-15 11:29:07 -0600

    Finalize and export the Story component

--- Frontend/src/components/StoryScreens/CardStory.js
+++ Frontend/src/components/StoryScreens/CardStory.js
@@ -2,25 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Story = ({ story }) => {
-    // Additional code will be added in subsequent commits...
-}
-    // Continuing from Commit 1...
+
     const editDate = (createdAt) => {
-        // Function implementation...
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+        ];
+        const d = new Date(createdAt);
+        var datestring = d.getDate() + " " +monthNames[d.getMonth()] + " ," + d.getFullYear() 
+        return datestring
     }
 
     const truncateContent = (content) => {
-        // Function implementation...
+        const trimmedString = content.substr(0, 73);
+        return trimmedString
     }
-
-    const truncateTitle = (title) => {
-        // Function implementation...
+    const truncateTitle= (title) => {
+        const trimmedString = title.substr(0, 69);
+        return trimmedString
     }
-    // Further code will be added in subsequent commits...
-    // Continuing from Commit 2...
+    
     return (
+
         <div className="story-card">
-            // JSX structure and rendering logic...
+            <Link to={`/story/${story.slug}`} className="story-link">
+
+                <img className=" story-image" src={`/storyImages/${story.image}`} alt={story.title} />
+                <div className="story-content-wrapper">
+
+                    <h5 className="story-title">
+                        
+                    {story.title.length > 76 ? truncateTitle(story.title)+"..." : story.title
+                    
+                    }
+                    </h5>
+
+
+                    <p className="story-text"dangerouslySetInnerHTML={{__html : truncateContent( story.content) +"..."}}>
+                        </p>
+                    <p className="story-createdAt">{editDate(story.createdAt)} 
+                    </p>
+                </div>
+            </Link>
         </div>
-    );
+
+    )
 }
+
+export default Story;
