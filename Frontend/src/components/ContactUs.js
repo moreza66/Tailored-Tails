cb039bb7076656f3508938d7f4f4553e314a5424
“momemar” <mmemar@logisx.net>
2023-11-15 12:04:53 -0600

    Initialize ContactUs component with essential imports

---
+++ Frontend/src/components/ContactUs.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import Loader from './GeneralScreens/Loader';
+import { useNavigate } from 'react-router-dom';
+
+const ContactUs = () => {
+    // Additional code will be added in subsequent commits...
+}
