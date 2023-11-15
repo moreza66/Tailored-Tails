c9958d8dc6c829d2789d0b298d6b67cfc28f5f6a
“momemar” <mmemar@logisx.net>
2023-11-14 21:04:40 -0600

    Initialize Profile component with states and utility function

--- Frontend/src/components/ProfileScreens/Profile.js
+++ Frontend/src/components/ProfileScreens/Profile.js
@@ -5,3 +5,19 @@ import { Link, useNavigate } from 'react-router-dom';
 import Loader from "../GeneralScreens/Loader";
 import { AuthContext } from '../../Context/AuthContext';
 import { FiArrowLeft } from 'react-icons/fi'
+
+const Profile = () => {
+    const { config } = useContext(AuthContext)
+    const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(true)
+
+    const editDate = (createdAt) => {
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        const d = new Date(createdAt);
+        var datestring = d.getDate() + " " + monthNames[d.getMonth()] + " , " + d.getFullYear()
+        return datestring
+    }
+
+    const navigate = useNavigate()
