a15a5876f1e7269bc82d7e96eee9a973a06ac997
“momemar” <mmemar@logisx.net>
2023-11-15 12:10:02 -0600

    Set up state variables in AuthContextProvider

--- Frontend/src/Context/AuthContext.js
+++ Frontend/src/Context/AuthContext.js
@@ -6,3 +6,13 @@ export const AuthContext = React.createContext();
 const AuthContextProvider = props => {
     // Additional code will be added in subsequent commits...
 }
+    // Continuing from Commit 1...
+    const [activeUser, setActiveUser] = useState({});
+    const [config, setConfig] = useState({
+        headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+    });
+    // Further code will be added in subsequent commits...
+
