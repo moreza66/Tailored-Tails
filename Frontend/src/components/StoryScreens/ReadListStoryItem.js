ac45186db9a59ff855dd2475b453f5c05f2de173
“momemar” <mmemar@logisx.net>
2023-11-15 11:41:08 -0600

    Initialize ReadListStoryItem component with imports

---
+++ Frontend/src/components/StoryScreens/ReadListStoryItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { AiFillStar } from 'react-icons/ai';
+import { BsThreeDots, BsBookmarkFill } from 'react-icons/bs';
+
+const ReadListStoryItem = ({ story, editDate }) => {
+    // Further code will be added in subsequent commits...
+}
