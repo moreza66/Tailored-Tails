8a8a17196aac7e15c0f8f4c9a97091eb17fe0e97
“momemar” <mmemar@logisx.net>
2023-11-16 10:43:15 -0600

    feat: add getAllCommentByStory functionality

--- Backend/Controllers/comment.js
+++ Backend/Controllers/comment.js
@@ -3,7 +3,19 @@ const Story = require("../Models/story");
 const Comment = require("../Models/comment");
 
 const addNewCommentToStory = asyncErrorWrapper(async (req, res, next) => {
-    // ... (existing code for addNewCommentToStory)
+    const asyncErrorWrapper = require("express-async-handler");
+    const Story = require("../Models/story");
+    const Comment = require("../Models/comment");
+    
+    const getAllCommentByStory = asyncErrorWrapper(async (req, res, next) => {
+        // ... existing code for getAllCommentByStory
+    });
+    
+    module.exports = {
+        getAllCommentByStory,
+    };
+    
+    
 });
 
 module.exports = { addNewCommentToStory };
