746801a0e0d8bb44ba7d007a3d84a801f953ce95
“momemar” <mmemar@logisx.net>
2023-11-16 10:50:03 -0600

    add functionality to create a new story

---
+++ Backend/Controllers/story.js
@@ -0,0 +1,33 @@
+const asyncErrorWrapper = require("express-async-handler");
+const Story = require("../Models/story");
+const deleteImageFile = require("../Helpers/Libraries/deleteImageFile");
+
+const addStory = asyncErrorWrapper(async (req, res, next) => {
+    const { title, content } = req.body;
+    var wordCount = content.trim().split(/\s+/).length;
+    let readtime = Math.floor(wordCount / 200);
+
+    try {
+        const newStory = await Story.create({
+            title,
+            content,
+            author: req.user._id,
+            image: req.savedStoryImage,
+            readtime
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "Add a story successfully!",
+            data: newStory
+        });
+    } catch (error) {
+        deleteImageFile(req);
+        return next(error);
+    }
+});
+
+module.exports = {
+    addStory,
+    // other exports...
+};
