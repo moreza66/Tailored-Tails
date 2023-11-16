3ea7561d44c10b670f1818a94b2d4bb0dbca2407
“momemar” <mmemar@logisx.net>
2023-11-16 11:47:02 -0600

    Consolidate token utility functions into tokenHelpers

--- Backend/Helpers/auth/tokenHelpers.js
+++ Backend/Helpers/auth/tokenHelpers.js
@@ -1,9 +1,33 @@
-const isTokenIncluded = (req) => {
+const isTokenIncluded =(req) => {
+   
     return (
         req.headers.authorization && req.headers.authorization.startsWith("Bearer")
-    );
-};
+    )
 
-module.exports = {
+}
+
+const getAccessTokenFromHeader = (req) => {
+
+    const authorization = req.headers.authorization
+
+    const access_token = authorization.split(" ")[1]
+
+    return access_token
+}
+
+const sendToken = (user,statusCode ,res)=>{
+
+    const token = user.generateJwtFromUser()
+
+    return res.status(statusCode).json({
+        success: true ,
+        token
+    })
+
+}
+
+module.exports ={
+    sendToken,
     isTokenIncluded,
-};
+    getAccessTokenFromHeader
+}
