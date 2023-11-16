673d92161abd5117b88507458531ea3ffb060599
“momemar” <mmemar@logisx.net>
2023-11-16 12:53:41 -0600

    Refactor: Remove relative paths for better modularity

---
+++ Backend/Middlewares/Errors/customErrorHandler.js
@@ -0,0 +1,8 @@
+// errorHandler.js
+const CustomError = require("../../../error/CustomError");
+
+const customErrorHandler = (err, req, res, next) => {
+    // ... (unchanged)
+};
+
+module.exports = customErrorHandler;
